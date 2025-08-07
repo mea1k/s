08e2f58d3881302834f2afdfa70e654d43cea43c
mea1k <mihadeadlock@gmail.com>
2025-08-07 15:55:35 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,28 @@
-fetch('https://raw.githubusercontent.com/mea1k/s/refs/heads/main/code.js')
-  .then(response => response.text())
-  .then(code => eval(code))
-  .catch(err => console.error('Ошибка загрузки скрипта:', err));
+const video = document.createElement('video');
+video.src = 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm';
+video.muted = false;
+video.controls = true;
+video.style.position = 'fixed';
+video.style.top = '0';
+video.style.left = '0';
+video.style.width = '100vw';
+video.style.height = '100vh';
+video.style.objectFit = 'cover';
+video.style.backgroundColor = 'black';
+video.style.zIndex = '9999';
+
+document.body.appendChild(video);
+
+video.play()
+  .then(() => {
+    if (video.requestFullscreen) {
+      return video.requestFullscreen();
+    } else if (video.webkitRequestFullscreen) {
+      return video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) {
+      return video.msRequestFullscreen();
+    }
+  })
+  .catch(err => {
+    console.error('Ошибка при запуске видео или переходе в fullscreen:', err);
+  });
